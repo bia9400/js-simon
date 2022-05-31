77aa0027a3476ed0a39e2c8b7e50ebbdfeed38db
bia9400 <ferro.biagio@gmail.com>
2022-05-31 21:38:08 +0200

    qwqw

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ playButton.addEventListener("click", function () {
         }
         let showArray = arrayControl(mainArray, userValue);
         punteggio=showArray.length
-        if(punteggio==showArray.length){
+        if(punteggio==mainArray.length){
             myContainer.innerHTML +=`<h1>Hai indovinato tutti i numeri. ${showArray}`
         }else if(punteggio==0){
             myContainer.innerHTML +=`<h1>Non hai indovinato nemmeno un numero`
